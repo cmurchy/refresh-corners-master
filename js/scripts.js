35df5647e94a645d885fbe51030adf5f897fc93a
Charlie <cmurchy@engr-134-09.eastern.ewu.edu>
2014-10-20 13:21:08 -0700

    Final

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,15 @@ $(document).ready(function(){
   
   // Your Scripts
 
+// default 10px round corner
+$(one).corner();
+
+// 30px round corner
+$(two).corner('30px');
+
+// 15px bevel corner
+$(three).corner('15px bevel');
+
+
+
 });
